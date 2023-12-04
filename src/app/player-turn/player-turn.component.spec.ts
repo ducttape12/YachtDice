2ba8070efc01dfe82f1237d6a26498fa0fcb1e66
Keith Ott <ducttape12@yahoo.com>
2023-12-03 20:05:26 -0600

    Added DOM test

--- src/app/player-turn/player-turn.component.spec.ts
+++ src/app/player-turn/player-turn.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlayerTurnComponent } from './player-turn.component';
+import { YachtGame } from '../yacht-game';
 
 describe('PlayerTurnComponent', () => {
   let component: PlayerTurnComponent;
@@ -10,14 +11,21 @@ describe('PlayerTurnComponent', () => {
     await TestBed.configureTestingModule({
       imports: [PlayerTurnComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(PlayerTurnComponent);
     component = fixture.componentInstance;
+    component.yachtGame = new YachtGame(1);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have Player 1 displayed', () => {
+    const playerTurnComponent: HTMLElement = fixture.nativeElement;
+    const h1 = playerTurnComponent.querySelector('h1');
+    expect(h1?.textContent).toContain('Player 1');
+  })
 });
