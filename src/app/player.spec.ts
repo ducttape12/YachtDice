f9c5f58d3bc0f3036a427b0d4695e89917efee50
Keith Ott <ducttape12@yahoo.com>
2023-12-03 20:43:26 -0600

    Tests for score card full

--- src/app/player.spec.ts
+++ src/app/player.spec.ts
@@ -5,4 +5,47 @@ describe('Player', () => {
   it('should create an instance', () => {
     expect(new Player('test')).toBeTruthy();
   });
+
+  it('should list score card as not full when not all values have been recorded', () => {
+    // Arrange
+    const player = new Player('1');
+    player.onesScore = 0;
+    player.twosScore = 0;
+    player.threesScore = 0;
+    player.fivesScore = 0;
+    player.sixesScore = 0;
+    player.fullHouseScore = 0;
+    player.littleStraightScore = 0;
+    player.bigStraightScore = 0;
+    player.yachtScore = 0;
+
+    // Act
+    const scoreCardFull = player.scoreCardFull();
+
+    // Assert
+    expect(scoreCardFull).toBeFalse();
+  });
+
+  it('should list score card as full when all values have been recorded', () => {
+    // Arrange
+    const player = new Player('1');
+    player.onesScore = 0;
+    player.twosScore = 0;
+    player.threesScore = 0;
+    player.foursScore = 0;
+    player.fivesScore = 0;
+    player.sixesScore = 0;
+    player.fullHouseScore = 0;
+    player.fourOfAKindScore = 0;
+    player.littleStraightScore = 0;
+    player.bigStraightScore = 0;
+    player.choiceScore = 0;
+    player.yachtScore = 0;
+
+    // Act
+    const scoreCardFull = player.scoreCardFull();
+
+    // Assert
+    expect(scoreCardFull).toBeTrue();
+  })
 });
